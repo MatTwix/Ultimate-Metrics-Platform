a5d97e96443d09247ffa0414566fe009de597ccb
Matvey Fedorov <mfgolden@yandex.ru>
2025-11-05 18:14:44 +0300

    feat(loggers): update logging level to Info for production environment

--- services/notification-service/pkg/logger/logger.go
+++ services/notification-service/pkg/logger/logger.go
@@ -27,7 +27,7 @@ func New(env string) Logger {
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	default:
 		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	}
